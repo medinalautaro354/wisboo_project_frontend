6415cd0074dc8fce687429da90abe6c5a7432b3b
lautaro <medinalautaro354@gmail.com>
2020-08-16 22:58:03 -0300

    Close #05 - Create url service

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,11 +1,18 @@
 angular.module('wisboo')
-.controller('mainCtrl', ['$scope', function($scope){
+.controller('mainCtrl', ['$scope', 'urlServices', function($scope, urlservices){
     $scope.url = '';
-
+    $scope.urls = [];
 
     $scope.onSubmit = (e) => {
         e.preventDefault();
+        if($scope.form.$valid){
+            urlservices.post($scope.url).then((response)=>{
+                console.log(response);
+            });
 
-        console.log($scope.url);
+            $scope.url = '';
+        }
     }
+
+
 }]);
---
+++ app/services/urlservices.js
@@ -0,0 +1,25 @@
+
+
+angular.module('wisboo')
+.factory('urlServices', ['$http',function($http){
+
+    return {
+        "post": (url) =>{
+
+            return $http.post('http://localhost:3000/urls',{
+                originalUrl: url
+            })
+            .then((response) =>{
+                return response.data;
+            });
+        },
+        "getAll":() =>{
+
+            return $http.get('http://localhost:3000/urls')
+            .then((response) =>{
+                debugger
+                return response.data;
+            });
+        }
+    };
+}]);
